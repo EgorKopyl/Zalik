ac08a54ea15eadc6cfcc64cbc8e10e80237a3196
EgorKopyl <91937091+EgorKopyl@users.noreply.github.com>
2022-06-16 21:39:54 +0300

    Add files via upload

---
+++ Furniture/Furniture/Boat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace boat
+{
+    class Boat
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int TypeBoatId { get; set; }
+        public TypeBoat TypeBoat { get; set; }
+        public List<OrderBoat> OrderBoat { get; set; }
+        public List<Order> Order { get; set; }
+    }
+}
